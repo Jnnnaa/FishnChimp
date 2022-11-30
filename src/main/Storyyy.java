2446a4ee3c04b44e2776bc37803e841ac5ac05b7
Jenna <Jennaahlvers@t-online.de>
2022-11-30 15:00:35 +0100

    1

--- src/main/Storyyy.java
+++ src/main/Storyyy.java
@@ -2,5 +2,19 @@ package main;
 
 public class Storyyy {
 
+    public void textAttheBeginning(){
+
+    }
+    //Anfangstext, der nachdem der Spieler erstellt ist erscheint
+    // Auswahl ob die Geschichte begonnen werden soll
+    //Möglichkeiten zu Station 1 zu gehen (oder Waffen zu kaufen)
+
+    // 1.Station: "" MONSTER
+        //
+    //Gewonnen: zu Station 2 oder speichern und schließen
+    //2.Station: ""MONSTER
+        //
+
+
 
 }
