b0e301b3f0cec946783f65e51a17f65e7a97a080
eric <eric@sodemaenner.de>
2022-11-30 13:46:37 +0100

    Basic damage-calculation for npc

--- src/npc/Npc.java
+++ src/npc/Npc.java
@@ -2,6 +2,7 @@ package npc;
 
 public abstract class Npc {
     private int health;
+    private int armor;
 
     public Npc() {
 
@@ -11,9 +12,22 @@ public abstract class Npc {
         return health;
     }
 
-    public void reduceHealth(int reductionValue) {
-        health -= reductionValue;
+    /**
+     * Handles the case, a npc gets damaged.
+     * @param reductionValue the damage the npc receives
+     * @return how much the health was reduced
+     */
+    public int reduceHealth(int reductionValue) {
+        if (health - reductionValue <= 0) {
+            health = 0;
+            return health;
+        } else {
+            health -= reductionValue;
+            return reductionValue;
+        }
     }
 
-
+    public int damageAfterArmorReduction(int damage) {
+        return (int) (damage - (damage*(20*Math.log10((armor/3)+1)/100)));
+    }
 }
